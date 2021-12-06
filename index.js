ca3e0a5d4ea97d6f349d5db56bf343a14346aab0
Boring-insaan <harsh20011999@gmail.com>
2021-12-07 00:39:18 +0530

    fitness

--- index.js
+++ index.js
@@ -5,32 +5,40 @@ const bmiResult = document.querySelector(".bmi-result");
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
-  
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
+function openLoginForm() {
+  document.getElementById("LoginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+  document.getElementById("LoginForm").style.display = "none";
+}
+
 const calculateBMI = () => {
-  const height = parseInt(heightInput.value)/100;
+  const height = parseInt(heightInput.value) / 100;
   const weight = parseInt(weightInput.value);
-  if(isNaN(height) || isNaN(weight)){
+  if (isNaN(height) || isNaN(weight)) {
     bmiResult.innerHTML = "Enter all values correctly";
     heightInput.value = "";
     weightInput.value = "";
-  }else if(weight>150 || weight<=0){
+  } else if (weight > 150 || weight <= 0) {
     bmiResult.innerHTML = "Enter Weight Correctly";
     heightInput.value = "";
     weightInput.value = "";
-  }else if(height>1.95  || height<=0){
+  } else if (height > 1.95 || height <= 0) {
     bmiResult.innerHTML = "Enter Height Correctly";
     heightInput.value = "";
     weightInput.value = "";
-  }else{
-    const bmi = Math.round(weight/(height*height));
+  } else {
+    const bmi = Math.round(weight / (height * height));
     bmiResult.innerHTML = `Your BMI is: ${bmi}`;
-    heightInput.value = height*100;
+    heightInput.value = height * 100;
     weightInput.value = weight;
   }
-  
+
 }
 calculateButton.addEventListener("click", calculateBMI)
