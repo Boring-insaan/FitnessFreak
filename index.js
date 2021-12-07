31c50b9e126dd9c43dbee6883536999edb2a8342
Boring-insaan <harsh20011999@gmail.com>
2021-12-07 13:39:31 +0530

    fitness

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const calculateButton = document.querySelector(".calculate-button");
 const heightInput = document.querySelector(".heightValue");
 const weightInput = document.querySelector(".weightValue");
 const bmiResult = document.querySelector(".bmi-result");
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbar = document.getElementsByClassName("navbar")[0];
+
+toggleButton.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+})
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
